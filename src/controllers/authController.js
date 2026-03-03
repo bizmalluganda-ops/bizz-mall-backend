dc424221480bb03f74e6957cdb1f2f36faef919d
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2026-03-03 13:33:04 +0300

    Addlogout route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -100,3 +100,26 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("admin_token", {
+      httpOnly: true,
+      secure: isProduction ? true : false,
+      sameSite: "none",
+      path: "/",
+    });
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: isProduction ? true : false,
+      sameSite: isProduction ? "strict" : "lax",
+      path: "/",
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to logout",
+    });
+  }
+};
